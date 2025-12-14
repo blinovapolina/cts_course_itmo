5dd6e53966b5308021d71edab9bc25f63909ed8e
mariafedorova <maryfedorova2309@mail.ru>
2025-12-14 22:00:54 +0300

    Fixed lab_3/part_1

--- lab_3/part_1/tests/index.test.js
+++ lab_3/part_1/tests/index.test.js
@@ -1,15 +1,15 @@
-import { calculate } from '../src/index.js';
+import { calculate } from "../src/index.js"
 
-describe('calculate', () => {
-  test('should add two positive numbers', () => {
-    expect(calculate(2, 3)).toBe(5);
-  });
+describe("calculate", () => {
+  test("Тест сложения положительных чисел", () => {
+    expect(calculate(2, 3)).toBe(5)
+  })
 
-  test('should add negative numbers', () => {
-    expect(calculate(-2, -3)).toBe(-5);
-  });
+  test("Тест сложения отрицательных чисел", () => {
+    expect(calculate(-2, -3)).toBe(-5)
+  })
 
-  test('should add positive and negative numbers', () => {
-    expect(calculate(5, -3)).toBe(2);
-  });
-});
+  test("Тест сложения положительных и отрицательных чисел", () => {
+    expect(calculate(5, -3)).toBe(2)
+  })
+})
